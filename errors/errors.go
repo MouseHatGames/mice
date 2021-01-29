c51a9dd8ee33ea9d81e38cd14e2bc4ad79b73d86
Felipe Martinez <pipernene@gmail.com>
2021-01-30 00:24:50 +0100

    Fix error functions signatures

--- errors/errors.go
+++ errors/errors.go
@@ -1,21 +1,21 @@
 package errors
 
-func BadRequest(format string, a ...string) error {
-	return NewError(400, format, a)
+func BadRequest(format string, a ...interface{}) error {
+	return NewError(400, format, a...)
 }
 
-func Unauthorized(format string, a ...string) error {
-	return NewError(401, format, a)
+func Unauthorized(format string, a ...interface{}) error {
+	return NewError(401, format, a...)
 }
 
-func Forbidden(format string, a ...string) error {
-	return NewError(403, format, a)
+func Forbidden(format string, a ...interface{}) error {
+	return NewError(403, format, a...)
 }
 
-func NotFound(format string, a ...string) error {
-	return NewError(404, format, a)
+func NotFound(format string, a ...interface{}) error {
+	return NewError(404, format, a...)
 }
 
-func InternalServerError(format string, a ...string) error {
-	return NewError(404, format, a)
+func InternalServerError(format string, a ...interface{}) error {
+	return NewError(404, format, a...)
 }
